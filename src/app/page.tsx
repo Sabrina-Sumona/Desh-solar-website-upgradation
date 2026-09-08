570a0a39c3037609c71052cc6f5a8397cccb8ee8
Sabrina-Sumona <sabrina.naorin.sumona@gmail.com>
2026-09-08 15:24:20 +0600

    refactor: remove redundant solar solution categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import CinematicIntro from "@/components/home/CinematicIntro";
 import HomeGateway from "@/components/home/HomeGateway";
 import EnergyJourney from "@/components/home/EnergyJourney";
 import LiveEnergyStory from "@/components/home/LiveEnergyStory";
-import SolutionCategories from "@/components/home/SolutionCategories";
 import MiniSystemBuilder from "@/components/home/MiniSystemBuilder";
 
 import "@/styles/home-cinematic.css";
 import "@/styles/home-gateway.css";
 import "@/styles/home-energy-journey.css";
 import "@/styles/home-live-energy-story.css";
-import "@/styles/home-solution-categories.css";
 import "@/styles/home-mini-system-builder.css";
 import FeaturedProjects from "@/components/home/FeaturedProjects";
 import "@/styles/home-featured-projects.css";
@@ -24,8 +22,6 @@ export default function HomePage() {
       <EnergyJourney />
 
       <LiveEnergyStory />
-
-      <SolutionCategories />
 
       <MiniSystemBuilder />
 
