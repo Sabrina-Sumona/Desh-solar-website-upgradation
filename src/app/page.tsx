245803033fa32fd705dc4a6c111937937b9a4f55
Sabrina-Sumona <sabrina.naorin.sumona@gmail.com>
2026-09-08 15:51:02 +0600

    feat: add solar system technology overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ import "@/styles/home-live-energy-story.css";
 import "@/styles/home-mini-system-builder.css";
 import FeaturedProjects from "@/components/home/FeaturedProjects";
 import "@/styles/home-featured-projects.css";
+import TechnologyStack from "@/components/home/TechnologyStack";
+import "@/styles/home-technology-stack.css";
 
 export default function HomePage() {
   return (
@@ -26,6 +28,8 @@ export default function HomePage() {
       <MiniSystemBuilder />
 
       <FeaturedProjects />
+
+      <TechnologyStack />
     </>
   );
 }
